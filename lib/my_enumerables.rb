ccb5ead7d6ebc1a05247928e85da080e663921c0
Betsue Weldemariam <btsegay0@gmail.com>
2022-10-04 19:28:18 +0300

    my_each method completed

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,20 @@
 module Enumerable
   # Your code goes here
+  def my_inject
+
+  end
+
+  def my_map
+
+  end
+
+  def my_none
+
+  end
+
+  def my_select
+
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +23,25 @@ end
 # to this method
 class Array
   # Define my_each here
+
+  def my_all?
+    
+  end
+
+  def my_any?
+
+  end
+
+  def my_count
+
+  end
+
+  def my_each(&block)
+    each { |item| block.call item}
+  end
+
+  def my_each_with_index
+
+  end
+
 end
